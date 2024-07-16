f7d9210d65211fc159d64c380985b0790b62e481
fransedor <fransedoravelino@gmail.com>
2024-07-16 21:57:43 +0700

    added message, room, and room users in migration sql

--- app/repository/migrations/migration.sql
+++ app/repository/migrations/migration.sql
@@ -10,3 +10,22 @@ CREATE TABLE "users" (
   "created_at" timestamp
 	"role" roles
 );
+
+CREATE TABLE "message" {
+	"id" serial PRIMARY KEY,
+	"user_id" int,
+	"room_id" int,
+	"text" varchar,
+	"media_url" varchar,
+	"created_at" timestamp,
+}
+
+CREATE TABLE "room" {
+	"id" serial PRIMARY KEY,
+	"name" varchar,
+}
+
+CREATE tABLE "room_users" {
+	"user_id" int,
+	"room_id" int
+}
